4741b652521d22c168e509ded88f6cc4a8decf7a
Zendachromian <103535913+Zendachromian@users.noreply.github.com>
2023-12-17 23:57:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
 const addToCartButtons = document.querySelectorAll('.product button');
 
+// Define a product object constructor
+function Product(name, image, price) {
+  this.name = name;
+  this.image = image;
+  this.price = price;
+}
+
+// Initialize an empty cart array
+let cart = [];
+
+// Handle product addition to cart
+function addToCart(product) {
+  cart.push(product);
+  updateCartUI(product); // Update cart UI on addition
+  console.log(`Added product "${product.name}" to cart.`);
+}
+
+// Update cart UI based on added product
+function updateCartUI(product) {
+  // Implement logic to update cart UI, such as displaying confirmation message, updating badge, etc.
+  // For example:
+  const cartCountElement = document.getElementById('cart-count');
+  cartCountElement.innerText = cart.length;
+}
+
+// Bind click event to all "Add to Cart" buttons
 addToCartButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        // Add product to your cart logic here
-        alert('Product added to cart!');
-    });
+  const productElement = button.parentNode; // Assuming button is inside a product element
+  const productName = productElement.querySelector('h3').textContent;
+  const productImage = productElement.querySelector('img').src;
+  const productPrice = parseFloat(productElement.querySelector('p').textContent.slice(7)); // Extract price from "Price: $" format
+  const product = new Product(productName, productImage, productPrice);
+
+  button.addEventListener('click', () => addToCart(product));
 });
 
-window.onload = function() {
-    alert("Welcome to My eCommerce Website!");
+// Remove welcome alert (optional)
+if (window.onload) {
+  window.onload = null; // Disable existing welcome alert
 }
